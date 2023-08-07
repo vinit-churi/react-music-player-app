f584284bb89067e1479f1415438ab1108904d66b
Vidyesh Churi <vidyesh95@gmail.com>
2023-08-07 12:38:25 +0530

    setup project 🎬

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "@/components/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+    errorElement: <h1>Root error</h1>,
+    children: [
+      {
+        index: true,
+        element: <h1>Home</h1>,
+        errorElement: <h1>Root error</h1>,
+      },
+      {
+        path: "album/:albumId",
+        errorElement: <h1>album error</h1>,
+        element: <h1>Album</h1>
+      },
+      {
+        path: "artist/:artistId",
+        errorElement: <h1>artist error</h1>,
+        element: <h1>Artist</h1>
+      },
+      {
+        path: "user/:userId",
+        errorElement: <h1>user profile Root error</h1>,
+        children: [
+          {
+            path: "profile",
+            element : <h1>Profile home</h1>,
+            errorElement: <h1>user/profile error</h1>,
+          },
+          {
+            path: "settings",
+            errorElement: <h1>profile settings error</h1>,
+            element: <h1>Profile settings</h1>
+          },
+          {
+            path: "collection",
+            errorElement: <h1>collection error</h1>,
+            children : [
+              // => Recent
+              {
+                path: "recent",
+                errorElement: <h1>recent error</h1>,
+                element: <h1>Recent</h1>
+              },
+              // => favorites
+              {
+                path: "favorites",
+                errorElement: <h1>favorites error</h1>,
+                element: <h1>Favorites</h1>
+              },
+              //  => playlists
+              {
+                path: "playlists",
+                errorElement: <h1>playlist error</h1>,
+                element: <h1>Playlists</h1>,
+                children: [
+                  {
+                    path: ":playlistId",
+                    errorElement: <h1>playlist error</h1>,
+                    element: <h1> specific Playlist</h1>,
+                  }
+                ]
+              }
+            ]
+          },
+        ]
+      },
+      {
+        path: "*",
+        element: <h1>Not found</h1>
+      }
+    ]
+  }
+]);
+function App() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
