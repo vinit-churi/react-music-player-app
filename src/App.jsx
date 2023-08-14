1d2ab26b080bff6bcedbce5c0d40e360f7c4c441
vinit-churi <vinitchuri@hotmail.com>
2023-08-14 13:15:25 +0530

    some tests

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import RootLayout from "@/components/Root";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout/>,
+    element: <RootLayout />,
     errorElement: <h1>Root error</h1>,
     children: [
       {
@@ -15,12 +15,17 @@ const router = createBrowserRouter([
       {
         path: "album/:albumId",
         errorElement: <h1>album error</h1>,
-        element: <h1>Album</h1>
+        element: <h1>Album</h1>,
+      },
+      {
+        path: "search/",
+        errorElement: <h1>search error</h1>,
+        element: <h1>Search page</h1>,
       },
       {
         path: "artist/:artistId",
         errorElement: <h1>artist error</h1>,
-        element: <h1>Artist</h1>
+        element: <h1>Artist</h1>,
       },
       {
         path: "user/:userId",
@@ -28,29 +33,29 @@ const router = createBrowserRouter([
         children: [
           {
             path: "profile",
-            element : <h1>Profile home</h1>,
+            element: <h1>Profile home</h1>,
             errorElement: <h1>user/profile error</h1>,
           },
           {
             path: "settings",
             errorElement: <h1>profile settings error</h1>,
-            element: <h1>Profile settings</h1>
+            element: <h1>Profile settings</h1>,
           },
           {
             path: "collection",
             errorElement: <h1>collection error</h1>,
-            children : [
+            children: [
               // => Recent
               {
                 path: "recent",
                 errorElement: <h1>recent error</h1>,
-                element: <h1>Recent</h1>
+                element: <h1>Recent</h1>,
               },
               // => favorites
               {
                 path: "favorites",
                 errorElement: <h1>favorites error</h1>,
-                element: <h1>Favorites</h1>
+                element: <h1>Favorites</h1>,
               },
               //  => playlists
               {
@@ -62,24 +67,22 @@ const router = createBrowserRouter([
                     path: ":playlistId",
                     errorElement: <h1>playlist error</h1>,
                     element: <h1> specific Playlist</h1>,
-                  }
-                ]
-              }
-            ]
+                  },
+                ],
+              },
+            ],
           },
-        ]
+        ],
       },
       {
         path: "*",
-        element: <h1>Not found</h1>
-      }
-    ]
-  }
+        element: <h1>Not found</h1>,
+      },
+    ],
+  },
 ]);
 function App() {
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-stone-100 col-start-2 col-end-3 row-start-1 row-end-2">
+    <nav className="bg-slate-100 col-start-2 col-end-3 row-start-1 row-end-2">
       <p>nav</p>
     </nav>
   );
