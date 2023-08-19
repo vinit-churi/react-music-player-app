bfc457fadb71d30f0465c57d73dec59180227e93
vinit-churi <vinitchuri@hotmail.com>
2023-08-19 08:42:49 +0530

    nav style ✨

--- src/components/Navbar/NavUserProfile.jsx
+++ src/components/Navbar/NavUserProfile.jsx
@@ -1,11 +1,13 @@
 const NavUserProfile = () => {
   let user = null;
   return (
-    <div>
+    <div className="flex items-center">
       {user ? (
         <div aria-label="user profile dropdown"></div>
       ) : (
-        <button>login / Sign-up</button>
+        <button className="h-10 rounded-full border-2 border-slate-700 px-5 bg-yellow-200 mx-5">
+          login / Sign-up
+        </button>
       )}
     </div>
   );
--- src/components/Navbar/SearchInput.jsx
+++ src/components/Navbar/SearchInput.jsx
@@ -6,10 +6,14 @@ const SearchInput = () => {
     console.log(event.target.value);
   }
   return (
-    <div>
-      <form onSubmit={handleSearch} className="">
+    <div className="flex gap-5 items-center flex-[1_1_auto]">
+      <form
+        onSubmit={handleSearch}
+        className="flex gap-2 flex-[1_1_auto] max-w-[325px] items-center bg-white rounded-full p-[0px_20px_0px_5px] h-10 focus-within:outline-2 focus-within:outline-slate-700 focus-within:ring-2 "
+      >
+        <img src={searchIcon} alt="" />
         <input
-          className=""
+          className="border-non outline-none h-full flex-[1_1_auto]"
           type="text"
           name="search"
           id="search"
@@ -17,11 +21,17 @@ const SearchInput = () => {
           aria-label="Search music input"
           aria-required="true"
         />
-        <img src={searchIcon} alt="" />
       </form>
-      <div className="" aria-label="search options">
+      <div
+        className="flex gap-2 items-center rounded-full h-10 border-2 border-slate-700 px-3 cursor-pointer"
+        aria-label="search options"
+      >
         <p>Songs</p>
-        <img src={dorpDownIcon} alt="drop down" />
+        <img
+          src={dorpDownIcon}
+          alt="drop down"
+          className="relative top-[2px]"
+        />
       </div>
     </div>
   );
