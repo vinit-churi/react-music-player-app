44007c7795dd448278c3993bd85ebabcf5900d56
vinit-churi <vinitchuri@hotmail.com>
2023-09-05 13:02:36 +0530

    song queue style

--- src/components/SongsQueue/index.jsx
+++ src/components/SongsQueue/index.jsx
@@ -1,25 +1,45 @@
-import { useState, useEffect, useRef } from "react";
-const SongsQueue = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const intervalRef = useRef(null);
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      // setIsExpanded((prev) => !prev);
-    }, 5000);
+import { FaExpandAlt } from "react-icons/fa";
+import { GiMusicSpell } from "react-icons/gi";
+import { useSelector, useDispatch } from "react-redux";
 
-    return () => {
-      clearInterval(intervalRef.current);
-    };
-  }, []);
+import {
+  selectExpandedQueue,
+  setExpandedQueue,
+} from "@/features/audioPlayer/audioPlayerSlice";
+const SongsQueue = () => {
+  const expandedQueue = useSelector(selectExpandedQueue);
+  const dispatch = useDispatch();
+
   return (
-    <section className="col-start-3 col-end-4 row-start-2 row-end-3 bg-pink-300 w-min">
+    <section className="col-start-3 col-end-4 row-start-2 row-end-3 w-min">
       <div
-        className={`overflow-hidden h-full bg-blue-100 transition-all p-2 duration-300 ease-in-out grid grid-rows-[300px] justify-items-center content-center ${
-          !isExpanded ? "w-[100px]" : "w-[300px]"
-        }`}
+        className={`overflow-hidden h-full bg-slate-100 transition-all p-2 duration-300 ease-in-out grid ${
+          expandedQueue ? "grid-rows-[100%]" : "grid-rows-[350px]"
+        } justify-items-center content-center 
+        w-max`}
       >
-        <div className={`w-[100%] bg-red-400 rounded-md`}>
-          <p>song queue section</p>
+        <div
+          className={` relative bg-white group rounded-md hover:w-[300px] transition-all ease-in-out duration-300 ${
+            expandedQueue ? "w-[300px]" : "w-[90px]"
+          }`}
+        >
+          {/* <GiMusicSpell/> */}
+          <p
+            className={`w-[90px] group-hover:hidden p-2 h-auto bg-slate-200 text-center font-bold text-lg text-slate-700 transition-all ease-in-out duration-300 rounded-tr-md rounded-tl-md ${
+              expandedQueue ? "hidden" : "block"
+            }`}
+          >
+            Queue
+          </p>
+          <div
+            onClick={() => dispatch(setExpandedQueue(!expandedQueue))}
+            className={`group-hover:flex absolute  items-center justify-center top-4 right-4 bg-slate-300  w-[25px] h-[25px] rounded-full cursor-pointer ${
+              expandedQueue ? "flex bg-yellow-200" : "hidden"
+            }`}
+          >
+            <FaExpandAlt className="h-2/3 w-2/3 object-contain" />
+          </div>
+          {/* <p>song queue section</p> */}
         </div>
       </div>
     </section>
