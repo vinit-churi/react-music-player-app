734c18bf02556431b4735e204b97fdfc050d5070
Vinit Churi <vinitchuri@hotmail.com>
2023-09-14 23:43:06 +0530

    modified some

--- src/features/music/SongCard.jsx
+++ src/features/music/SongCard.jsx
@@ -48,20 +48,20 @@ const SongCard = ({ song, index }) => {
         className="flex-[0_0_min-content] h-6 mx-2 cursor-pointer hover:scale-105 hover:text-yellow-400 transition-all ease-in-out duration-300"
       />
       {currentTrack?.id === song.id ? (
-        <div className="group  h-12 w-12 mx-1 flex justify-center items-center">
+        <div className="group flex-[0_0_max-content] mr-3  h-12 w-12 mx-1 flex justify-center items-center">
           <img
             src={animatedIcon}
-            className="flex-[0_0_min-content] invert-0 h-2/3 w-2/3 object-center object-contain group-hover:hidden cursor-pointer hover:scale-105 hover:text-yellow-400 transition-all ease-in-out duration-300"
+            className="flex-[0_0_max-content]  h-8 w-8 object-center object-contain group-hover:hidden cursor-pointer hover:scale-105 "
           />
           {isPlaying ? (
             <BsFillPauseCircleFill
               onClick={() => dispatch(pause())}
-              className="hidden group-hover:block object-contain object-center h-6 w-6 cursor-pointer"
+              className="hidden group-hover:block object-contain object-center h-8 w-8 cursor-pointer hover:text-cyan-400 transition-all ease-in-out duration-300"
             />
           ) : (
             <AiFillPlayCircle
               onClick={() => dispatch(play())}
-              className="hidden group-hover:block object-contain object-center h-6 w-6 cursor-pointer hover:scale-105 hover:text-yellow-400 transition-all ease-in-out duration-300"
+              className="hidden group-hover:block object-contain object-center h-8 w-8 cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all ease-in-out duration-300"
             />
           )}
         </div>
